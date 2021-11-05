16ab4673747d5357bed6d44a9254f9f26d215a39
ngoulongkam <25244131+ngoulongkam@users.noreply.github.com>
2021-11-05 13:40:20 +0000

    initial commit

---
+++ app/src/main/java/com/example/qcard/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.qcard
+
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.qcard.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var frontAnim: AnimatorSet
+    private lateinit var backAnim: AnimatorSet
+    private lateinit var binding: ActivityMainBinding
+    private var isFront = true
+
+    private val listOfWordAndDescription = listOf(
+        WordDescription("Abyssinian", "Coat colour: Reddish-brown with black ticking"),
+        WordDescription("Balinese", "Coat colour: Dark brown, blue, chocolate, lilac"),
+        WordDescription("Bengal", "Coat colour: Brown, snow, silver, blue"),
+        WordDescription("Birman", "Coat colour: Seal, blue, lilac, chocolate, red, cream"),
+        WordDescription("British", "Coat colour: white, black, blue, lilac, chocolate, red (single colour)")
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val scale = applicationContext.resources.displayMetrics.density
+        binding.cardFront.cameraDistance = 8000 * scale
+        binding.cardBack.cameraDistance = 8000 * scale
+
+        frontAnim = AnimatorInflater.loadAnimator(applicationContext, R.animator.front_animator) as AnimatorSet
+        backAnim = AnimatorInflater.loadAnimator(applicationContext, R.animator.back_animator) as AnimatorSet
+
+        binding.flipButton.setOnClickListener {
+            if (isFront) flipFrontToBack()  else flipBackToFront()
+        }
+
+        binding.nextButton.setOnClickListener {
+            if (!isFront) flipBackToFront()
+            setNextWordAndDescription()
+        }
+    }
+
+    private fun flipFrontToBack() {
+        frontAnim.setTarget(binding.cardFront)
+        backAnim.setTarget(binding.cardBack)
+        frontAnim.start()
+        backAnim.start()
+        isFront = false
+    }
+
+    private fun flipBackToFront() {
+        frontAnim.setTarget(binding.cardBack)
+        backAnim.setTarget(binding.cardFront)
+        backAnim.start()
+        frontAnim.start()
+        isFront = true
+    }
+
+    private fun setNextWordAndDescription() {
+        val randomIndex = Random.nextInt(listOfWordAndDescription.size)
+        val randomWord = listOfWordAndDescription[randomIndex].word
+        val randomDescription = listOfWordAndDescription[randomIndex].desc
+        binding.cardFront.text = randomWord
+        binding.cardBack.text = randomDescription
+    }
+
+    data class WordDescription(
+        val word: String,
+        val desc: String
+    )
+}
